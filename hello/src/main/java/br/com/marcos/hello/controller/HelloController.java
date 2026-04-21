8cc93f9ebe59115550a25d32bedd5f57f7000ca9
gitrodrigues1 <rodrigues.ramos@gmail.com>
2026-04-20 22:18:54 -0300

    Add initial Spring Boot application with Maven wrapper and configuration files

---
+++ hello/src/main/java/br/com/marcos/hello/controller/HelloController.java
@@ -0,0 +1,15 @@
+package br.com.marcos.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable() String name) {
+        return "Hello " + name; 
+    }
+}
